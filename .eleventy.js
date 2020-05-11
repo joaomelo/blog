24db1462d443a4b561378d74cc174f6a4826228c
joao <j@vmelo.net>
2020-05-11 20:45:07 -0300

    experimental version

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,23 @@
-module.exports = {
-  dir: {
-    input: "src",
-    output: "dist"
+const moment = require('moment');
+moment.locale('en');
+
+module.exports = function (eleventyConfig) {
+ 
+  eleventyConfig.addFilter('dateIso', date => {
+    return moment(date).toISOString();
+  });
+ 
+  eleventyConfig.addFilter('dateReadable', date => {
+    return moment(date).format('LL'); // E.g. May 31, 2019
+  });
+
+  eleventyConfig.addPassthroughCopy("css");
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    }
   }
 };
+
