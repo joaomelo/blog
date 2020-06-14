39047ada3862643208c71cb0b373d98acc453626
joao <j@vmelo.net>
2020-06-14 20:18:10 -0300

    prrof of concept

--- config/files.js
+++ config/files.js
@@ -1,9 +1,12 @@
 module.exports = function configFiles(eleventyConfig) {
   // enable image copying
-  eleventyConfig.setTemplateFormats(["njk", "md", "gif", "png"]);
+  eleventyConfig.setTemplateFormats(["njk", "md"]);
 
   // css dir
   eleventyConfig.addPassthroughCopy("src/styles");
+
+  // media
+  eleventyConfig.addPassthroughCopy("src/media");
 
   // github cname
   eleventyConfig.addPassthroughCopy("src/CNAME");
